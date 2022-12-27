f8d6e418aa9cb54fc06cc50740c138571d5d2224
srnyx <25808801+srnyx@users.noreply.github.com>
2022-12-26 23:57:02 -0500

    New `actions.sh` file for Actions

---
+++ Tools/backend/actions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+version="${{ inputs.version }}"
+packwiz="../../Tools/backend/packwiz.exe"
+
+for dir in $version; do
+  if [[ -d $dir ]]; then
+    cd "$dir"
+    $packwiz modrinth export
+    $packwiz curseforge export
+    break
+  fi
+done
